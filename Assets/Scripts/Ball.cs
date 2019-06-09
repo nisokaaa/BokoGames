a6783f93e7aaadd58dba2a2e203d8ec5fdf594c8
nisokaaa <www.nisoka@gmail.com>
2019-06-09 22:56:29 +0900

    RayCast2Dの当たり判定・同色の球を消す処理実装

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,26 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Ball : MonoBehaviour
 {
+    /** 一意なID */
+    public string m_Id;
+
     /** 情報 */
-    BallInfo m_Info;
+    public BallInfo m_Info;
 
     /** 2DTransform */
+    [SerializeField]
     RectTransform m_Rect;
+
+    /** 2DRayCast */
+    [SerializeField]
+    RayHit2d m_RayCast;
 
     void Start ()
     {
-		
-	}
+    }
 	
 	void Update ()
     {
+        
     }
 
+    public void OnTouch()
+    {
+        // 結果リスト
+        List<Ball> results = new List<Ball>();
+        results.Add(this);
+
+        // 走査無視リスト
+        List<string> ignores = new List<string>();
+        ignores.Add(m_Id);
+
+        // 走査開始
+        SearchContact(this, ref ignores, ref results);
+        
+        // 結果出力
+        foreach (Ball ball in results)
+        {
+            ball.gameObject.SetActive(false);
+            DebugManager.Log(ball.m_Info.type.ToString());
+        }
+        DebugManager.Log("------------------------------", Color.red);
+    }
+
+    /// <summary>
+    /// 同じ色かつ無視リストに載っていないオブジェクトを走査する関数
+    /// </summary>
+    /// <param name="obj">衝突判定が行われるオブジェクト</param>
+    /// <param name="ignores">走査無視リストの参照</param>
+    /// <param name="results">結果リストの参照</param>
+    void SearchContact(Ball obj, ref List<string> ignores, ref List<Ball> results)
+    {
+        // 衝突判定があったオブジェクトを取得
+        List<Ball> balls = obj.m_RayCast.GetHitBalls(obj);
+
+        if(balls != null)
+        {
+            foreach (Ball ball in balls)
+            {
+                // 走査無視リストに載っていたら無視して次へ
+                if (ignores.Contains(ball.m_Id))
+                {
+                    continue;
+                }
+
+                // 違う色なら無視して次へ
+                if (ball.m_Info.type != m_Info.type)
+                {
+                    continue;
+                }
+
+                // 目的の球を結果リスト・走査無視リストに追加
+                results.Add(ball);
+                ignores.Add(ball.m_Id);
+
+                // 再帰的に走査
+                SearchContact(ball, ref ignores, ref results);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 初期化関数
+    /// </summary>
+    /// <param name="position">初期位置</param>
+    /// <param name="info">ボール情報</param>
     public void Init(Vector2 position, BallInfo info)
     {
+        // 一意なID設定
+        m_Id = Guid.NewGuid().ToString("N").Substring(0, 5);
+
         // ボール情報をロード
         m_Info = info;
 
@@ -29,7 +105,7 @@ public class Ball : MonoBehaviour
         GetComponent<Image>().sprite = m_Info.sprite;
 
         // 2D座標を設定
-        m_Rect = GetComponent<RectTransform>();
         m_Rect.position = position;
     }
+    
 }
---
+++ Assets/Scripts/RayHit2d.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayHit2d : MonoBehaviour
+{
+    class Ray
+    {
+        public Ray(Vector2 origin, float theta)
+        {
+            this.origin = origin;
+
+            // オブジェクトを中心とした2DRayの向きを計算
+            this.direction = (origin - Vector2.zero).normalized;
+
+            this.theta = theta;
+        }
+
+        /** Rayの開始位置 */
+        public Vector2 origin;
+
+        /** Rayの向き */
+        public Vector2 direction;
+
+        /** シータ */
+        public float theta;
+    }
+
+    /** Rayリスト */
+    List<Ray> m_RayList = new List<Ray>();
+
+    /** Rayの数 */
+    [SerializeField]
+    int m_Amount = 25;
+
+    /** Rayの長さ */
+    [SerializeField]
+    float m_Length = 25f;
+
+    /** 中心座標からRay開始座標までの半径R */
+    [SerializeField]
+    float m_OriginOffsetR = 30f;
+
+    /** 親の2DTransforｍ */
+    [SerializeField]
+    RectTransform m_ParentRect;
+
+    /** HitしたBallのリスト */
+    List<Ball> m_HitBalls = new List<Ball>();
+
+    void Start()
+    {
+        for (int i = 0; i < m_Amount; i++)
+        {
+            // 度数をラジアン角に変換
+            float theta = (360f / m_Amount * i) * Mathf.PI / 180f;
+
+            // オブジェクトの中心から半径Rだけ離れた2DRayの開始座標を求める
+            Vector2 origin = new Vector2(m_OriginOffsetR * Mathf.Cos(theta),
+                                      m_OriginOffsetR * Mathf.Sin(theta));
+
+            m_RayList.Add(new Ray(origin, theta));
+        }
+    }
+
+    void Update()
+    {
+        foreach(Ray ray in m_RayList)
+        {
+            // オブジェクトの中心から半径Rだけ離れた2DRayの開始座標を求める
+            ray.origin = new Vector2(m_OriginOffsetR * Mathf.Cos(ray.theta),
+                                      m_OriginOffsetR * Mathf.Sin(ray.theta));
+
+            Debug.DrawRay(new Vector2(ray.origin.x + m_ParentRect.position.x, ray.origin.y + m_ParentRect.position.y), ray.direction * m_Length, Color.green, 0.01f);
+        }
+    }
+
+    /// <summary>
+    /// RayにHitしたBallのリストを返す
+    /// </summary>
+    /// <returns>Ballのリスト</returns>
+    public List<Ball> GetHitBalls(Ball ignore)
+    {
+        List<string> m_IgnoreIds = new List<string>();
+
+        m_HitBalls.Clear();
+
+        foreach (Ray ray in m_RayList)
+        {
+            RaycastHit2D info = Physics2D.Raycast(new Vector2(ray.origin.x + m_ParentRect.position.x,
+                                                              ray.origin.y + m_ParentRect.position.y),
+                                                  ray.direction, m_Length);
+
+            // RayCastが衝突物を検知し、それが壁ではない場合
+            if (info.collider != null && info.collider.tag != "wall")
+            {
+                Ball ball = info.collider.GetComponent<Ball>();
+
+                if(ball.m_Id == ignore.m_Id)
+                {
+                    continue;
+                }
+
+                // 無視リストが空なら
+                if (m_IgnoreIds.Count == 0)
+                {
+                    // 走査無視リストに追加
+                    m_IgnoreIds.Add(ball.m_Id);
+
+                    // 衝突リストに追加
+                    m_HitBalls.Add(ball);
+
+                    continue;
+                }
+
+                for(int i = 0; i < m_IgnoreIds.Count; i++)
+                {
+                    // 衝突物が無視リストに含まれていないなら
+                    if (!m_IgnoreIds.Contains(ball.m_Id))
+                    {
+                        // 走査無視リストに追加
+                        m_IgnoreIds.Add(ball.m_Id);
+
+                        // 衝突リストに追加
+                        m_HitBalls.Add(ball);
+                    }
+                }    
+            }
+        }
+
+        //// デバッグ表示
+        //foreach(Ball ball in m_HitBalls)
+        //{
+        //    DebugManager.Log(ball.m_Info.type.ToString());
+        //}
+        //DebugManager.Log("------------------------------", Color.red);
+
+        return m_HitBalls.Count > 0 ? m_HitBalls : null;
+    }
+}
